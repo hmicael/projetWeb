8bc2c174ea2b6c452ba5f8efb123a6a0be4cd45b
henintsoa <handriamahadimby@gmail.com>
2023-03-03 21:33:43 +0100

    modifier lien du boutton modifier

--- views/matieres.php
+++ views/matieres.php
@@ -24,7 +24,7 @@
                         echo '<td>' . ucfirst($value) . '</td>';
                     }
                     echo '<td>';
-                        echo '<a href="index.php?action=admin">Modifier</a>';
+                        echo '<a href="index.php?action=admin&edit=matieres&id=' . $id . '">Modifier</a>';
                         echo '<a href="index.php?action=admin&delete=matieres&id=' . $id . '">Supprimer</a>';
                     echo '</td>';
                 echo '</tr>';
