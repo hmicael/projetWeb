275c3fee7e175c27a6dd1501b7fc72d1136c6224
henintsoa <handriamahadimby@gmail.com>
2023-03-15 16:06:38 +0100

    hash password

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -9,7 +9,7 @@ $jsonString = file_get_contents(WEBROOT. '/data/utilisateurs.json');
 $utilisateurs = json_decode($jsonString, true);
 echo '<pre>';
 foreach ($utilisateurs as $key => $u) {
-    if($u['email'] == htmlspecialchars($_POST['email']) && $u['password'] == htmlspecialchars($_POST['password'])) {
+    if($u['email'] == htmlspecialchars($_POST['email']) && password_verify(htmlspecialchars($_POST['password']), $u['password'])) {
         // si les crendentials sont conforme, on quitte la boucle en se redirigeant vers la page d'accueil
         $_SESSION['id'] = $key;
         $_SESSION['role'] = strtolower($u['role']);
